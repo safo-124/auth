0b2bc62599365ddbcad3c272763648670e510df0
Safo Emmanuel <nanasafo122@gmail.com>
2024-01-18 11:50:43 +0000

    first commit

---
+++ components/auth/card-wrapper.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
+import { BackButton } from "./back-button";
+import { Header } from "./header";
+import { Social } from "./social";
+
+interface CardWarpperProps {
+    children: React.ReactNode;
+    headerLabel: string;
+    backButtonLabel: string;
+    backButtonHref: string;
+    showSocial?: boolean;
+};
+
+export const CardWarpper = ({
+    children, headerLabel, backButtonLabel, showSocial, backButtonHref
+}: CardWarpperProps) => {
+    return (
+        <Card className="w-[400px] shadow-md">
+            <CardHeader>
+                <Header label={headerLabel}/>
+            </CardHeader>
+            <CardContent>
+                {children}
+            </CardContent>
+            {showSocial && (
+                <CardFooter>
+                    <Social/>
+                </CardFooter>
+            )}
+            <CardFooter>
+               <BackButton
+                label={backButtonLabel}
+                href={backButtonHref}
+               /> 
+            </CardFooter>
+        </Card>
+    )
+}
