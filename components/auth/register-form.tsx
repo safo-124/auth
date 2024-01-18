1ede497167c022c610e43dc6ec8617e3e739864a
Safo Emmanuel <nanasafo122@gmail.com>
2024-01-18 13:49:25 +0000

    second commit

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -4,34 +4,35 @@ import { CardWarpper } from "./card-wrapper";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
-import { LoginSchema } from "@/schemas";
+import { RegisterSchema } from "@/schemas";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { FormError } from "../form-error";
 import { FormSuccess } from "../form-success";
-import { login } from "@/actions/login";
+import { register } from "@/actions/register";
 import { useState, useTransition } from "react";
 
-export const LoginForm = () => {
+export const RegisterForm = () => {
     const [error, setError] = useState<string | undefined>("")
     const [success, setSuccess] = useState<string | undefined>("")
     const [isPending, startTransition] = useTransition();
 
-    const form = useForm<z.infer<typeof LoginSchema>>({
-        resolver: zodResolver(LoginSchema),
+    const form = useForm<z.infer<typeof RegisterSchema>>({
+        resolver: zodResolver(RegisterSchema),
         defaultValues: {
             email: "",
             password: "",
+            name:"",
         }
     })
 
-    const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
         setError("");
         setSuccess("");
 
         startTransition(() => {
-            login(values).then((data) =>{
+            register(values).then((data) =>{
                 setError(data.error);
                 setSuccess(data.success)
             })
@@ -40,9 +41,9 @@ export const LoginForm = () => {
 
     return (
         <CardWarpper
-            headerLabel="Welcome Back"
-            backButtonLabel="Don't have an account"
-            backButtonHref="/auth/register"
+            headerLabel="Create an account"
+            backButtonLabel="Already have an account"
+            backButtonHref="/auth/login"
             showSocial
         >
             <Form {...form}>
@@ -50,6 +51,24 @@ export const LoginForm = () => {
                     className="space-y-6"
                 >
                     <div className="space-y-4">
+                        <FormField
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Name</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            disabled={isPending}
+                                            placeholder="John Doe"
+                                            
+                                        />
+                                    </FormControl>
+                                    <FormMessage/>
+                                </FormItem>
+                            )}
+                        />
                         <FormField
                             control={form.control}
                             name="email"
@@ -68,6 +87,7 @@ export const LoginForm = () => {
                                 </FormItem>
                             )}
                         />
+                        
                         <FormField
                             control={form.control}
                             name="password"
@@ -93,7 +113,7 @@ export const LoginForm = () => {
                        type="submit"
                        className="w-full" 
                     >
-                        Login
+                        Create an account
                     </Button>
                 </form>
             </Form>
